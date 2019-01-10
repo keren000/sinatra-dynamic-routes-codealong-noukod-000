dcb1ca210bc8f9a941f3176161d75a299bbfd5a4
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-10 18:02:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,6 +14,11 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+  get "/multiply/:number1/:number2" do
+  end
+  
   get "/goodbye/:name" do
-end
+    @person_name = params[:name]
+    "Goodbye #{@person_name}."
+  end
 end
